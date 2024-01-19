8e97bc0d1e53b50f5a6be755bf1e25f3a18cfea3
Ono Keita <oh.kitbsk.1028@icloud.com>
2024-01-19 15:01:47 +0000

    ~ Chapter 19

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { ActionButton } from "./ActionButton";
+import { FormDialog } from "./FormDialog";
+import { SideBar } from "./SideBar";
+import { TodoItem } from "./TodoItem";
+
+export const App = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filter>("all");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+      checked: false,
+      removed: false,
+    };
+
+    setTodos((todos) => [newTodo, ...todos]);
+    setText("");
+  };
+
+  const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
+    id: number,
+    key: K,
+    value: V
+  ) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, [key]: value };
+        } else {
+          return todo;
+        }
+      });
+
+      return newTodos;
+    });
+  };
+
+  const handleSort = (filter: Filter) => {
+    setFilter(filter);
+  };
+
+  const handleEmpty = () => {
+    setTodos((todos) => todos.filter((todo) => !todo.removed));
+  };
+
+  return (
+    <div>
+      <SideBar onSort={handleSort} />
+      <FormDialog text={text} onChange={handleChange} onSubmit={handleSubmit} />
+      <TodoItem todos={todos} filter={filter} onTodo={handleTodo} />
+      <ActionButton todos={todos} onEmpty={handleEmpty} />
+    </div>
+  );
+};
